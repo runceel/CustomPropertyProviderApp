0935d68d6ddab77b6b4162d446aa2cd9312ca41f
Kazuki Ota <kaota@microsoft.com>
2021-07-08 17:09:12 +0900

    add a command

---
+++ CustomPropertyProviderApp/DelegateCommand.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace CustomPropertyProviderApp
+{
+    public class DelegateCommand : ICommand
+    {
+        private readonly Action _execute;
+
+        public event EventHandler CanExecuteChanged;
+
+        public DelegateCommand(Action execute)
+        {
+            _execute = execute;
+        }
+
+        public bool CanExecute(object parameter) => true;
+
+        public void Execute(object parameter) => _execute();
+    }
+}
--- CustomPropertyProviderApp/MainPageViewModel.cs
+++ CustomPropertyProviderApp/MainPageViewModel.cs
@@ -1,28 +1,60 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Windows.Input;
 using Windows.UI.Xaml.Data;
 
 namespace CustomPropertyProviderApp
 {
-    public class MainPageViewModel
+    public class MainPageViewModel : INotifyPropertyChanged
     {
         private string _guid = System.Guid.NewGuid().ToString();
-        public string Guid => _guid;
+        public string Guid
+        {
+            get => _guid;
+            set
+            {
+                _guid = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Guid)));
+            }
+        }
+
+        private ICommand _updateGuidCommand;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ICommand UpdateGuidCommand => _updateGuidCommand ?? (_updateGuidCommand = new DelegateCommand(ExecuteUpdateGuid));
+
+        private void ExecuteUpdateGuid()
+        {
+            Guid = System.Guid.NewGuid().ToString();
+        }
     }
 
-    public class CustomPropertyProviderMainPageViewModel : ICustomPropertyProvider
+    public class CustomPropertyProviderMainPageViewModel : ICustomPropertyProvider, INotifyPropertyChanged
     {
         public Model Model { get; } = new Model { Guid = Guid.NewGuid().ToString() };
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public CustomPropertyProviderMainPageViewModel()
+        {
+            Model.PropertyChanged += Model_PropertyChanged;
+        }
+
+        private void Model_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(e.PropertyName));
+        }
+
         public ICustomProperty GetCustomProperty(string name)
         {
             if (name == nameof(CustomPropertyProviderApp.Model.Guid))
             {
                 return new GuidProperty();
+            }
+            else if (name == nameof(CustomPropertyProviderApp.Model.UpdateGuidCommand))
+            {
+                return new UpdateGuidCommandProperty();
             }
             else
             {
@@ -40,9 +72,61 @@ namespace CustomPropertyProviderApp
         public Type Type => typeof(Model);
     }
 
-    public class Model
+    public class Model : INotifyPropertyChanged
     {
-        public string Guid { get; set; }
+        private string _guid;
+        public string Guid
+        {
+            get => _guid;
+            set
+            {
+                _guid = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Guid)));
+            }
+        }
+
+        private ICommand _updateGuidCommand;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ICommand UpdateGuidCommand => _updateGuidCommand ?? (_updateGuidCommand = new DelegateCommand(ExecuteUpdateGuid));
+
+        private void ExecuteUpdateGuid()
+        {
+            Guid = System.Guid.NewGuid().ToString();
+        }
+    }
+
+    public class UpdateGuidCommandProperty : ICustomProperty
+    {
+        public object GetValue(object target)
+        {
+            var vm = (CustomPropertyProviderMainPageViewModel)target;
+            return vm.Model.UpdateGuidCommand;
+        }
+
+        public void SetValue(object target, object value)
+        {
+            throw new NotSupportedException();
+        }
+
+        public object GetIndexedValue(object target, object index)
+        {
+            throw new NotSupportedException();
+        }
+
+        public void SetIndexedValue(object target, object value, object index)
+        {
+            throw new NotSupportedException();
+        }
+
+        public bool CanRead => true;
+
+        public bool CanWrite => false;
+
+        public string Name => nameof(Model.UpdateGuidCommand);
+
+        public Type Type => typeof(ICommand);
     }
 
     public class GuidProperty : ICustomProperty
